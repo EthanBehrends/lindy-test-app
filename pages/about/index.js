c638ef6a7c66326c4e50180d84bd9efad333ad0f
EthanBehrends <epbehrends@gmail.com>
2024-03-05 22:12:07 -0800

    feat: add about us page

---
+++ pages/about/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our website. We are a team dedicated to providing the best services in our field.</p>
+    </div>
+  );
+}
